84600a1d09f433ce7ab02a92a459b2d1032311a2
Based Work Token <167148036+BasedWorkToken@users.noreply.github.com>
2025-01-14 18:17:46 -0600

    Update 1BasedWorkToken.sh

--- BWORK Miner Mainnet Ubuntu 22&24 v8.2.4/1BasedWorkToken.sh
+++ BWORK Miner Mainnet Ubuntu 22&24 v8.2.4/1BasedWorkToken.sh
@@ -1,7 +1,8 @@
 #!/usr/bin/env bash
 
 
-#Check for dotnet
+#Check for dotnet   # Verify installation
+   
 if ! command -v dotnet &> /dev/null
 then
     echo "dotnet 6.0 is not found or not installed."
@@ -19,11 +20,42 @@ then
     dotnet --version
 
     echo "dotnet 6.0 is installed. Rerun the script to start."
-
-    goto end
 else
-    echo "dotnet 6.0 is already installed."
+
+    dotnet_version=$(dotnet --version)
+    if [[ $dotnet_version == 6.* ]]
+    then
+    	echo "dotnet 6.0 is installed dont worry about installiing it"
+    else
+	echo "Installing dotnet 6.0 because it isnt installed"
+	
+	
+	
+	
+	echo "dotnet 6.0 is not found or not installed."
+	echo "Installing dotnet 6.0..."
+
+	# Add Microsoft package signing key and repository
+	wget https://packages.microsoft.com/config/ubuntu/$(lsb_release -rs)/packages-microsoft-prod.deb -O packages-microsoft-prod.deb
+	sudo dpkg -i packages-microsoft-prod.deb
+	sudo apt-get update; \
+	sudo apt-get install -y apt-transport-https && \
+	sudo apt-get update && \
+	sudo apt-get install -y dotnet-sdk-6.0
+
+	# Verify the installation
+	dotnet --version
+
+	echo "dotnet 6.0 is installed. Rerun the script to start."
+
+    
+    
+    
+    
+	exit 1
+    fi
 fi
+
 
 while : ; do
   dotnet BasedWorkToken.dll
